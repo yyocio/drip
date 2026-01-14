eb2f16822644a3681741420e8f9a269fcf86a31c
Gouryella <q6221745@gmail.com>
2026-01-14 15:41:54 +0800

    build(release): Ignore unsupported platform architecture combinations
    fix(tuning): Fix type conversion issue in memory calculation

--- internal/shared/tuning/mem_linux.go
+++ internal/shared/tuning/mem_linux.go
@@ -7,7 +7,7 @@ import "syscall"
 func getSystemTotalMemory() uint64 {
 	var info syscall.Sysinfo_t
 	if err := syscall.Sysinfo(&info); err == nil {
-		return info.Totalram * uint64(info.Unit)
+		return uint64(info.Totalram) * uint64(info.Unit)
 	}
 	return 1024 * 1024 * 1024
 }
